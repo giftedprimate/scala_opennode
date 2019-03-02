865005bc6a81d45ae0298f541b87053346dbc1e4
Richard Porter <richard@mathbot.com>
2019-03-02 18:43:58 -0500

    cleans up entities

---
+++ src/main/scala/com/github/scala_opennode/entities/WithdrawalResponseData.scala
@@ -0,0 +1,25 @@
+package com.github.scala_opennode.entities
+import play.api.libs.json.{Json, OFormat}
+
+object WithdrawalResponseData {
+  implicit val withdrawalResponseFormat: OFormat[WithdrawalResponseData] = Json.format[WithdrawalResponseData]
+}
+
+/**
+ * @param id Withdrawal ID
+ * @param `type` Withdrawal type
+ * @param amount Amount withdrew (satoshis)
+ * @param reference transaction id (chain) or payment request (In)
+ * @param processed_at Withdrawal processing timestamp
+ * @param address Withdrawal destination
+ * @param fee Withdrawal fee
+ */
+case class WithdrawalResponseData(
+    id: String,
+    `type`: String,
+    amount: Int,
+    reference: String,
+    processed_at: Int,
+    address: String,
+    fee: Int
+)
