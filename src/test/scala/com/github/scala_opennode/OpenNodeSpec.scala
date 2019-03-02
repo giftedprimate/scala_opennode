423e42e91587431230040790e8482aa8901c9f4a
Richard Porter <richard@mathbot.com>
2019-03-02 18:41:48 -0500

    specs pass

--- src/test/scala/com/github/scala_opennode/OpenNodeSpec.scala
+++ src/test/scala/com/github/scala_opennode/OpenNodeSpec.scala
@@ -1,22 +1,20 @@
 package com.github.scala_opennode
 
-import entities._
-import org.scalatest.concurrent.Waiters
-import org.scalatest.time.SpanSugar._
-import org.scalatestplus.play._
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.libs.json.{JsValue, Json, OFormat}
-import play.api.test.Injecting
-import scala.math.BigDecimal
-import sys.process._
-import scala.concurrent.ExecutionContext.Implicits.global
-import com.github.scala_opennode.OpenNode
+import com.github.scala_opennode.entities._
+import org.scalatest.AsyncFlatSpec
+import play.api.libs.json.{Json, OFormat}
+import org.apache.logging.log4j.scala.Logging
 
-class OpenNodeSpec extends PlaySpec with Waiters with GuiceOneAppPerSuite with Injecting {
-  val openNode = new OpenNode(sys.env("mathbot_payments_openNode_apiKey"))
+import scala.sys.process._
 
+object OpenNodeSpec {
   var chargeResponseDataOpt: Option[ChargeResponseData] = None
   var withdrawalId: Option[String] = None
+}
+
+class OpenNodeSpec extends AsyncFlatSpec with Logging {
+  import OpenNodeSpec._
+  val openNode = new OpenNode(sys.env("mathbot_payments_openNode_apiKey"))
 
   object Transaction {
     implicit val transactionFormat: OFormat[Transaction] = Json.format[Transaction]
@@ -40,44 +38,31 @@ class OpenNodeSpec extends PlaySpec with Waiters with GuiceOneAppPerSuite with I
       `bip125-replaceable`: Option[String],
       abandoned: Option[Boolean]
   )
-  "generateCharge" must {
-    "respond with correct data with proper inputs" in {
-      val wait = new Waiter
-      openNode.generateCharge(amount = 1,
-                              description = "unit test",
-                              customerEmail = "1@1.com",
-                              customerName = "fred",
-                              autoSettle = false) map {
-        case Left(chargeResponseData) =>
-          chargeResponseDataOpt = Some(chargeResponseData)
-          wait { assert(chargeResponseData.isInstanceOf[ChargeResponseData]) }
-          wait.dismiss()
-        case Right(_) =>
-          wait { fail() }
-          wait.dismiss()
-      }
-      wait.await(timeout(3.minutes))
-    }
 
-    "respond with OpenNodeError with bad input" in {
-      val wait = new Waiter
-      openNode.generateCharge(amount = 0,
-                              description = "unit test",
-                              customerEmail = "1@1.com",
-                              customerName = "fred",
-                              autoSettle = false) map {
-        case Left(_) =>
-          wait { fail() }
-          wait.dismiss()
-        case Right(openNodeError) =>
-          wait { assert(openNodeError.isInstanceOf[OpenNodeError]) }
-          wait.dismiss()
-      }
-      wait.await(timeout(3.seconds))
+  behavior of "generateCharge"
+  it should "respond with ChargeResponseData when given proper inputs" in {
+    for {
+      chargeResponseData <- openNode.generateCharge(amount = 753394,
+                                                    description = "unit test",
+                                                    customerEmail = "1@1.com",
+                                                    customerName = "fred",
+                                                    autoSettle = false)
+    } yield {
+      chargeResponseDataOpt = Some(chargeResponseData.left.get)
+      assert(chargeResponseData.isLeft && chargeResponseData.left.get.isInstanceOf[ChargeResponseData])
     }
   }
+  it should "respond with OpenNodeError with bad input" in {
+    for {
+      openNodeError <- openNode.generateCharge(amount = 0,
+                                               description = "unit test",
+                                               customerEmail = "1@1.com",
+                                               customerName = "fred",
+                                               autoSettle = false)
+    } yield assert(openNodeError.isRight && openNodeError.right.get.isInstanceOf[OpenNodeError])
+  }
 
-  "be able to pay charge" in {
+  "bitcoin-cli" should "be able to pay the charge" in {
     chargeResponseDataOpt match {
       case Some(chargeResponseData) =>
         val txId =
@@ -87,98 +72,52 @@ class OpenNodeSpec extends PlaySpec with Waiters with GuiceOneAppPerSuite with I
     }
   }
 
-  "getCharges" must {
-    "respond with correct data" in {
-      val wait = new Waiter
-      openNode.getCharges() map {
-        case Left(chargeInfoDatas) =>
-          wait { assert(chargeInfoDatas.isInstanceOf[List[ChargeInfoData]]) }
-          wait.dismiss()
-        case Right(_) =>
-          wait { fail() }
-      }
-      wait.await(timeout(3.seconds))
-    }
+  behavior of "getCharges"
+  it should "respond with List[ChargeInfoData]" in {
+    for {
+      chargeInfoData <- openNode.getCharges()
+    } yield assert(chargeInfoData.isLeft && chargeInfoData.left.get.isInstanceOf[List[ChargeInfoData]])
   }
 
-  "getCharge" must {
-    "respond with correct data if sent a valid id" in {
-      val wait = new Waiter
-      chargeResponseDataOpt match {
-        case Some(chargeResponseData) =>
-          openNode.getCharge(chargeResponseData.id) map {
-            case Left(chargeInfoData) =>
-              wait { assert(chargeInfoData.isInstanceOf[ChargeInfoData]) }
-              wait.dismiss()
-            case Right(_) =>
-              wait { fail() }
-          }
-        case None => wait { fail() }
-      }
-      wait.await(timeout(3.seconds))
-    }
-
-    "respond with OpenNodeError if sent an invalid id" in {
-      val wait = new Waiter
-      openNode.getCharge("not-an-id-at-all") map {
-        case Left(_) => wait { fail() }
-        case Right(openNodeError) =>
-          wait { assert(openNodeError.isInstanceOf[OpenNodeError]) }
-          wait.dismiss()
-      }
-      wait.await(timeout(3.seconds))
-    }
+  behavior of "getCharge"
+  it should "respond with ChargeInfoData when sent valid ID" in {
+    for {
+      chargeInfoData <- openNode.getCharge(chargeResponseDataOpt.get.id)
+    } yield assert(chargeInfoData.isLeft && chargeInfoData.left.get.isInstanceOf[ChargeInfoData])
+  }
+  it should "respond with OpenNodeError if sent invalid id" in {
+    for {
+      openNodeError <- openNode.getCharge("not-an-id")
+    } yield assert(openNodeError.isRight && openNodeError.right.get.isInstanceOf[OpenNodeError])
   }
 
-  "initiateWithdrawal" must {
-    "respond with correct data when sent property address" in {
-      val wait = new Waiter
-      val address = "bitcoin-cli getnewaddress".!!.trim
-      val amount = chargeResponseDataOpt.get.amount.get // using get here since earlier tests will fail if None
-      openNode.initiateWithdrawal(
+  behavior of "initiateWithdrawal"
+  it should "respond with WithdrawalResponseData when sent valid info" in {
+    val address = "bitcoin-cli getnewaddress".!!.trim
+    val amount = chargeResponseDataOpt.get.amount.get
+    for {
+      withdrawalResponseData <- openNode.initiateWithdrawal(
         `type` = "chain",
         amount = amount,
         address = address
-      ) map {
-        case Left(withdrawalResponseData) =>
-          withdrawalId = Some(withdrawalResponseData.id)
-          wait { assert(withdrawalResponseData.isInstanceOf[WithdrawalResponseData]) }
-          wait.dismiss()
-        case Right(_) =>
-          wait { fail() }
-      }
-      wait.await(timeout(3.seconds))
+      )
+    } yield {
+      withdrawalId = Some(withdrawalResponseData.left.get.id)
+      assert(withdrawalResponseData.isLeft && withdrawalResponseData.left.get.isInstanceOf[WithdrawalResponseData])
     }
   }
 
-  "getAllWithdrawals" must {
-    "respond with correct data" in {
-      val wait = new Waiter
-      openNode.getAllWithdrawals() map {
-        case Left(withdrawalInfoResponses) =>
-          wait { assert(withdrawalInfoResponses.isInstanceOf[List[WithdrawalInfoData]]) }
-          wait.dismiss()
-        case Right(_) => wait { fail() }
-      }
-      wait.await(timeout(3.seconds))
-    }
+  behavior of "getAllWithdrawals"
+  it should "respond with List[WithdrawalInfoData" in {
+    for {
+      withdrawalInfoData <- openNode.getAllWithdrawals()
+    } yield assert(withdrawalInfoData.isLeft && withdrawalInfoData.left.get.isInstanceOf[List[WithdrawalInfoData]])
   }
 
-  "withdrawalInfo" must {
-    "respond with correct data if sent a valid id" in {
-      val wait = new Waiter
-      val id = withdrawalId.get
-      openNode.withdrawalInfo(id) map {
-        case Left(chargeInfoData) =>
-          wait {
-            assert(chargeInfoData.isInstanceOf[WithdrawalInfoData])
-            assert(chargeInfoData.id == id)
-            assert(chargeInfoData.status.contains("pending") || chargeInfoData.status.contains("confirmed"))
-          }
-          wait.dismiss()
-        case Right(_) => wait { fail() }
-      }
-      wait.await(timeout(3.seconds))
-    }
+  behavior of "withdrawalInfo"
+  it should "respond with WithdrawalInfoData when sent a valid ID" in {
+    for {
+      withdrawalInfoData <- openNode.withdrawalInfo(withdrawalId.get)
+    } yield assert(withdrawalInfoData.isLeft && withdrawalInfoData.left.get.isInstanceOf[WithdrawalInfoData])
   }
 }
